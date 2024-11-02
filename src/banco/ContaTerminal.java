6730628f37206534cd0df0810c4f8f8d3fc2542f
Bia <ntenhodoritosmasdepende@gmail.com>
2024-11-01 21:50:27 -0300

    Projeto feito para o curso DIO

---
+++ src/banco/ContaTerminal.java
@@ -0,0 +1,33 @@
+    package banco;
+    
+    import java.util.Scanner;
+
+    public class ContaTerminal {
+        public static void main(String[] args) {
+            int numero;
+            String agencia;
+            String nome;
+            double saldo;
+            
+            try(Scanner ler = new Scanner(System.in)){
+                System.out.println("Por favor, digite o numero da Agencia: ");
+                numero = ler.nextInt();
+
+                System.out.println("Por favor, digite a Agencia: ");
+                agencia = ler.next();
+
+                ler.nextLine();
+
+                System.out.println("Por favor, digite o seu nome: ");
+                nome = ler.nextLine();
+
+                System.out.println("Por favor, digite o saldo: ");
+                saldo = ler.nextDouble();
+            }catch(Exception erro){
+                System.out.println("Ocorreu um erro na entrada de dados. Tente novamente!");
+                System.exit(0);
+            }
+
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero +  " e seu saldo " + saldo + " já está disponível para saque!");
+        }
+    }
